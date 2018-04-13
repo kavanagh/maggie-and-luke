f03e78de3b332d7c2b99365e62d0f0fa98b42692
Kavanagh, Luke <ltkavanagh@gmail.com>
2018-04-13 15:36:49 +0100

    No special error handler

--- server.js
+++ server.js
@@ -48,16 +48,8 @@ app.get('/', HomeController.index);
 app.get('/contact', contactController.contactGet);
 app.post('/contact', contactController.contactPost);
 
-// Production error handler
-if (app.get('env') === 'production') {
-  app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.sendStatus(err.status || 500);
-  });
-}
-
 app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Server listening on port ' + app.get('port'));
 });
 
 module.exports = app;
